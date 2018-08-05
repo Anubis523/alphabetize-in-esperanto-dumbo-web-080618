39582b3e08e2c49a957fe63420e96b2b5a131a3f
Kevin Steele <kevindsteeleii@gmail.com>
2018-08-05 14:07:22 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,4 +3,4 @@ def alphabetize(arr)
 arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
 
-puts alphabetize("hello my name is nothing")
+# puts alphabetize("hello my name is nothing")
