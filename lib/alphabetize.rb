bcb3731134f5d6f0d187c5eb4e11f58e9be741b4
Kevin Steele <kevindsteeleii@gmail.com>
2018-08-05 14:01:47 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,8 @@ def alphabetize(arr)
   # code here
   words = arr.split(' ')
   # words.class -> establishes the 
-  words.sort_by
+  words.sort_by do
+  end
 end
 
 puts alphabetize("hello my name is nothing")
